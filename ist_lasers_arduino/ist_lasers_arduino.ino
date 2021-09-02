12c3b59a9ee0718e40348d8d5ef320b9cf159578
quendera <tiagoquendera@gmail.com>
2021-09-02 02:16:37 +0100

    midi works! and initial ideas for full functioning

--- ist_lasers_arduino/ist_lasers_arduino.ino
+++ ist_lasers_arduino/ist_lasers_arduino.ino
@@ -2,27 +2,43 @@
   IST Lasers arduino setup
 */
 
-int laser1Pin = 9;    // Laser1 connected to digital pin 9
-int laser2Pin = 13;    // Laser2 connected to digital pin 13
+int laserAPin = 9;    // Laser1 connected to digital pin 9
+int laserBPin = 13;    // Laser2 connected to digital pin 13
 
 
-int laser1Pwm = 128;    // Laser 1 Pwm 1000mW ( 0 = off, 255 = 1000mW, 128 = 500mW)
-int laser2Pwm = 36;    // Laser 2 Pwm 3500mW ( 0 = off, 255 = 3500mW, 73 = 1000mW, 36 = 500mW)
+int laserAPwm = 128;    // Laser 1 Pwm 1000mW ( 0 = off, 255 = 1000mW, 128 = 500mW)
+int laserBPwm = 36;    // Laser 2 Pwm 3500mW ( 0 = off, 255 = 3500mW, 73 = 1000mW, 36 = 500mW)
 
 
-int x;
+String raw;
+int value;
+
+
 
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(1);
-  analogWrite(laser1Pin, laser1Pwm);
-  analogWrite(laser2Pin, laser2Pwm);
+  analogWrite(laserAPin, laserAPwm);
+  analogWrite(laserBPin, laserBPwm);
 
 }
 
 
 void loop() {
   while (!Serial.available());
-  x = Serial.readString().toInt();
-  Serial.print(x + 1);
+
+  raw = Serial.readString();
+  
+  if (raw.startsWith("A")) {
+    raw.remove(0,1);
+    value = raw.toInt();
+    analogWrite(laserAPin, value);
+  }
+  
+  else if (raw.startsWith("B")) {
+    raw.remove(0,1);
+    value = raw.toInt();
+    analogWrite(laserBPin, value);
+  }
+  
 }
